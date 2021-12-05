7a30edb4e3d9837fe9b26f83badcb7c0a3de19e6
andyzhou <andyforever0108@outlook.com>
2021-12-05 19:47:49 +0800

    docs: Update version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,18 +1,15 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/djask'))
 
-project = 'Djask'
-copyright = '2021, Andy Zhou'
-author = 'Andy Zhou'
-release = '0.1.0'
+sys.path.insert(0, os.path.abspath("../src/djask"))
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    "sphinx_tabs.tabs"
-]
-templates_path = ['_templates']
+project = "Djask"
+copyright = "2021, Andy Zhou"
+author = "Andy Zhou"
+release = "0.2.0"
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_tabs.tabs"]
+templates_path = ["_templates"]
 exclude_patterns = []
 autodoc_typehints = "description"
 intersphinx_mapping = {
@@ -20,5 +17,5 @@ intersphinx_mapping = {
 }
 
 html_theme = "alabaster"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_favicon = "_static/djask.ico"
