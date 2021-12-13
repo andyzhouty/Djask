6791c0c83fa8e3dc9da9d57144c62b297abd4bbf
andyzhou <andyforever0108@outlook.com>
2021-12-13 21:42:04 +0800

    ci(rtd): Correct readthedocs config

---
+++ setup.py
@@ -0,0 +1,62 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import codecs
+
+with codecs.open('README.md', encoding="utf-8") as fp:
+    long_description = fp.read()
+INSTALL_REQUIRES = [
+    'apiflask~=0.10',
+    'flask-sqlalchemy~=2.5',
+    'flask-wtf~=0.15',
+    'flask-login~=0.5',
+    'bootstrap-flask~=1.8',
+    'flask-compress~=1.10',
+    'wtforms-sqlalchemy~=0.3',
+]
+
+setup_kwargs = {
+    'name': 'Djask',
+    'version': '0.2.0',
+    'description': 'An enhanced django-like Flask',
+    'long_description': long_description,
+    'license': 'MIT',
+    'author': '',
+    'author_email': 'andyzhou <andyforever0108@outlook.com>',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'packages': [
+        'djask',
+        'djask.admin',
+        'djask.auth',
+        'djask.db',
+    ],
+    'package_dir': {'': 'src'},
+    'package_data': {'': ['*']},
+    'long_description_content_type': 'text/markdown',
+    'keywords': ['flask', 'apiflask'],
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    'install_requires': INSTALL_REQUIRES,
+    'python_requires': '>=3.7',
+
+}
+
+
+setup(**setup_kwargs)
