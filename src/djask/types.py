02f62c08ded56a5c8f49a150cde37e59474b4345
andyzhou <andyforever0108@outlook.com>
2022-08-04 15:13:28 +0800

    fix(typing): Fix typing in types.py

--- src/djask/types.py
+++ src/djask/types.py
@@ -1,18 +1,10 @@
-from __future__ import annotations
-
-from typing import Any
-from typing import Literal
-from typing import Mapping
-from typing import Sequence
-from typing import Tuple
-from typing import Type
-from typing import Union
+import typing as t
 
 from .db.models import Model
 
-Config = Any
-ErrorResponse = Union[Tuple[str, int], Tuple[str, int, Mapping[str, str]]]
-ModelType = Type[Model]
-ModelList = list[ModelType]
-ModeLiteral = Literal["api", "ui"]
-ModeArg = Tuple[ModeLiteral | Sequence[ModeLiteral]]
+Config = t.Any
+ErrorResponse = t.Union[t.Tuple[str, int], t.Tuple[str, int, t.Mapping[str, str]]]
+ModelType = t.Type[Model]
+ModelList = t.List[ModelType]
+ModeLiteral = t.Literal["api", "ui"]
+ModeArg = t.Tuple[t.Union[ModeLiteral, t.Sequence[ModeLiteral]]]
